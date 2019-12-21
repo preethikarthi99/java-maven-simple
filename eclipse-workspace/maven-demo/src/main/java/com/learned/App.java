3809ff8ef604a49688c5519321d16a744f73ce5d
preethikarthi99 <preethikarthikeyan99@gmail.com>
2019-12-21 23:59:38 +0530

    simple maven project added

---
+++ eclipse-workspace/maven-demo/src/main/java/com/learned/App.java
@@ -0,0 +1,11 @@
+package com.learned;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Maven project created");
+
+	}
+
+}
